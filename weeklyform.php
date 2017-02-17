9a14b36aa9a1e67ee69f605d5b2db029cc5fac3b
Turan Yildiz <t.yildiz1997@gmail.com>
2017-02-17 15:17:39 +0100

    Viewport toegevoegd aan weklyforms

--- weeklyform.php
+++ weeklyform.php
@@ -7,6 +7,7 @@ if(!isset($_COOKIE["visitor"])) {
 <html lang="nl">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <title>The Final Day</title>
         <link rel="icon" href="assets/img/favicon.png">
         <link rel="stylesheet" href="resources/css/style.css">
